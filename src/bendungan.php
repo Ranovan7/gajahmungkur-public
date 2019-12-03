8332980475cb574c77e768585c2c90db4771dcc0
Ranovan7 <romyranovan@gmail.com>
2019-12-03 16:42:41 +0700

    show all bendungan data

--- src/bendungan.php
+++ src/bendungan.php
@@ -8,8 +8,29 @@ $app->group('/bendungan', function() {
 
     $this->get('[/]', function(Request $request, Response $response, $args) {
 
+        $waduk = $this->db->query("SELECT waduk.*,
+                                            periodik_daily.outflow_deb as outflow_deb,
+                                            periodik_daily.spillway_deb as spillway_deb,
+                                            periodik_daily.curahhujan as curahhujan,
+                                            periodik_daily.outflow_vol as outflow_vol,
+                                            periodik_keamanan.debit as debit
+                                    FROM waduk
+                                    LEFT JOIN periodik_daily ON periodik_daily.id = (
+                                        SELECT id from periodik_daily
+                                            WHERE periodik_daily.waduk_id = waduk.id
+                                            ORDER BY sampling DESC
+                                            LIMIT 1
+                                    )
+                                    LEFT JOIN periodik_keamanan ON periodik_keamanan.id = (
+                                        SELECT id from periodik_keamanan
+                                            WHERE periodik_keamanan.waduk_id = waduk.id
+                                                AND keamanan_type = 'vnotch'
+                                            ORDER BY sampling DESC
+                                            LIMIT 1
+                                    )")->fetchAll();
+
         return $this->view->render($response, 'bendungan/index.html', [
-             'key' => 'value'
+             'waduk' => $waduk
         ]);
     })->setName('bendungan');
 
