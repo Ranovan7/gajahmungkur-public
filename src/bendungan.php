974c46d87e129387ff3468c73bff9a1467e6b0c2
Ranovan7 <romyranovan@gmail.com>
2019-12-04 14:22:02 +0700

    added individual waduk info, change query to fetch different rows into one row in /bendungan

--- src/bendungan.php
+++ src/bendungan.php
@@ -13,7 +13,10 @@ $app->group('/bendungan', function() {
                                             periodik_daily.spillway_deb as spillway_deb,
                                             periodik_daily.curahhujan as curahhujan,
                                             periodik_daily.outflow_vol as outflow_vol,
-                                            periodik_keamanan.debit as debit
+                                            periodik_keamanan.debit as debit,
+                                            tma_p.manual as tma6,
+                                            tma_s.manual as tma12,
+                                            tma_m.manual as tma18
                                     FROM waduk
                                     LEFT JOIN periodik_daily ON periodik_daily.id = (
                                         SELECT id from periodik_daily
@@ -27,6 +30,27 @@ $app->group('/bendungan', function() {
                                                 AND keamanan_type = 'vnotch'
                                             ORDER BY sampling DESC
                                             LIMIT 1
+                                    )
+                                    LEFT JOIN tma tma_p ON tma_p.id = (
+                                        SELECT id from tma
+                                            WHERE waduk_id = waduk.id
+                                                AND EXTRACT(HOUR FROM sampling) = '6'
+                                            ORDER BY sampling DESC
+                                            LIMIT 1
+                                    )
+                                    LEFT JOIN tma tma_s ON tma_s.id = (
+                                        SELECT id from tma
+                                            WHERE waduk_id = waduk.id
+                                                AND EXTRACT(HOUR FROM sampling) = '12'
+                                            ORDER BY sampling DESC
+                                            LIMIT 1
+                                    )
+                                    LEFT JOIN tma tma_m ON tma_m.id = (
+                                        SELECT id from tma
+                                            WHERE waduk_id = waduk.id
+                                                AND EXTRACT(HOUR FROM sampling) = '18'
+                                            ORDER BY sampling DESC
+                                            LIMIT 1
                                     )")->fetchAll();
 
         return $this->view->render($response, 'bendungan/index.html', [
@@ -34,11 +58,25 @@ $app->group('/bendungan', function() {
         ]);
     })->setName('bendungan');
 
-    $this->get('/{id}', function(Request $request, Response $response, $args) {
+    $this->group('/{id}', function() {
 
-        return $this->view->render($response, 'bendungan/info.html', [
-             'key' => 'value'
-        ]);
-    })->setName('bendungan.info');
+        $this->get('[/]', function(Request $request, Response $response, $args) {
+            $id = $request->getAttribute('id');
+            $waduk = $this->db->query("SELECT waduk.*,
+                                            tma.manual as tma
+                                        FROM waduk
+                                        LEFT JOIN tma ON tma.id = (
+                                            SELECT id from tma
+                                                WHERE waduk_id = waduk.id
+                                                ORDER BY sampling DESC
+                                                LIMIT 1
+                                        )
+                                        WHERE waduk.id={$id}")->fetch();
+
+            return $this->view->render($response, 'bendungan/info.html', [
+                 'waduk' => $waduk
+            ]);
+        })->setName('bendungan.info');
+    });
 
 });
